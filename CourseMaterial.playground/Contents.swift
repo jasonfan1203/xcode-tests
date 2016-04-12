c7c68523e0333f990722543669b4f8db88b16b28
jasonfan1203 <JasonFan@Jasons-MacBook-Pro.local>
2016-04-12 00:03:27 -0700

    test project modified

--- CourseMaterial.playground/Contents.swift
+++ CourseMaterial.playground/Contents.swift
@@ -2,9 +2,10 @@
 
 import UIKit
 
+// Function Lecture
+
 var bankAccount = 500.00
 var itemAmount = 199.95
-
 
 func canPurchase(amount: Double) -> Bool {
     if bankAccount >= amount {
@@ -24,3 +25,77 @@ if canPurchase(itemAmount) {
 } else {
     print("You're broke as shit, dude!")
 }
+
+
+// Exercise 1
+
+func add(a: Double, b:Double) -> Double {
+    return a + b
+}
+
+add(5, b: 10)
+
+// Exercise 2
+
+func subtract (a: Int, b: Int) -> Int {
+    return abs (a - b)
+}
+
+subtract(5, b: 100)
+
+// Exercise 3
+
+func multiply (a: Float, b: Float) -> Float {
+    return a * b
+}
+
+multiply(1.23, b: 2.55)
+
+// Exercise 4
+
+func divide (a: Double, b: Double) -> Double {
+    return a / b
+}
+
+divide(10, b: 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var bg1: UIImageView!
-    @IBOutlet weak var bg2: UIImageView!
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = [ "Who's a fucking retard?",
+                                "Who's a giant fuckhead?",
+                                "Who's a piece of dishit?"]
+    let answers: [String] = ["You",
+                            "Definitely you",
+                            "Da shit? It's you"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +46,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func changeBackground(sender: AnyObject) {
-        bg1.hidden = true
-        bg2.hidden = false
-    }
     
-    //this a comment to test git
-    
-    var test = 20
-     //okay?
+
 }
 
